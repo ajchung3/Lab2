81b1b3ac9c728b2a828196311855b2fb58f822c5
chung <ajchung3@illinois.edu>
2018-09-12 20:30:36 -0500

    1.0

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -34,7 +34,26 @@ public class SolveMaze {
          * Feel free to adjust this number if you experiment with other mazes.
          */
         for (int step = 0; step < 1000; step++) {
-            // Implement your maze solving algorithm here
+            while (!maze.isFinished()) {
+                maze.turnLeft(); // orient left
+                if (!maze.canMove()) { // check if can go left
+                    maze.turnRight();
+                    maze.turnRight();  // orient right
+                } else {
+                    maze.move();
+                }
+                if (!maze.canMove()) { // check if can go right
+                    maze.turnLeft(); // orient straight
+                } else {
+                    maze.move();
+                }
+                if (!maze.canMove()) { // check if can go straight
+                    maze.turnLeft();
+                    maze.turnLeft(); // orient backwards
+                    maze.move();
+                }
+            }
+
         }
 
         if (maze.isFinished()) {
